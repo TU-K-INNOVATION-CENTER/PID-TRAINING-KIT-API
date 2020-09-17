71ffce6ad40562ce7e07929f32b1d4a32b4d72bd
kibuikaCodes <kibuikasteve@gmail.com>
2020-09-17 12:16:29 +0300

    altered the dbname in the get routes

--- routes/get.js
+++ routes/get.js
@@ -1,16 +1,15 @@
-const express = require('express');
-const tukData = require('../models/data');
-
+const express = require("express");
+const pidData = require("../models/data");
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-    try{
-        const savedData = await tukData.find();
-        res.json(savedData);
-    } catch (error) {
-        res.json({message: error})
-    }
+router.get("/", async (req, res) => {
+  try {
+    const savedData = await pidData.find();
+    res.json(savedData);
+  } catch (error) {
+    res.json({ message: error });
+  }
 });
 
 module.exports = router;
